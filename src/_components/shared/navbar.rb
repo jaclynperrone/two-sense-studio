5c352b17a5e4ac167480fcc85d43240d7de5757e
Jaclyn Perrone <jaclynperrone@Jaclyns-MacBook-Pro.local>
2026-04-09 16:13:39 -0400

    More foundations, active nav class

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -2,4 +2,8 @@ class Shared::Navbar < Bridgetown::Component
   def initialize(metadata:, resource:)
     @metadata, @resource = metadata, resource
   end
+  def active_class?(path)
+    # Use @resource.relative_url to match Bridgetown's modern logic
+    "active" if @resource.relative_url == relative_url(path)
+  end
 end
