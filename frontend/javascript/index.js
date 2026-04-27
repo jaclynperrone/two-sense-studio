b2360104f5318cc456a209aea65fd9a64d18c0eb
Jaclyn Perrone <jaclyn.perrone@gmail.com>
2026-04-27 14:27:33 -0400

    Use sass, refactor grid layout

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,7 @@
-import "$styles/index.css"
-import "$styles/syntax-highlighting.css"
+import "$styles/index.scss"
+import "$styles/syntax-highlighting.scss"
 
 // Import all JavaScript & CSS files from src/_components
-import components from "$components/**/*.{js,jsx,js.rb,css}"
+import components from "$components/**/*.{js,jsx,js.rb,css,scss}"
 
 console.info("Bridgetown is loaded!")
