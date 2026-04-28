5d0b34a399af2f890072c8c4eea7237d60df59e5
Jaclyn Perrone <jaclyn.perrone@gmail.com>
2026-04-28 17:44:36 -0400

    Index and nav tweaks

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -4,6 +4,6 @@ class Shared::Navbar < Bridgetown::Component
   end
   def active_class?(path)
     # Use @resource.relative_url to match Bridgetown's modern logic
-    "active" if @resource.relative_url == relative_url(path)
+    "nav-item__active" if @resource.relative_url == relative_url(path)
   end
 end
